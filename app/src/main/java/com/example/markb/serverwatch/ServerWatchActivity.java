d1cf7ffaf5c5e4e56bce77fc2d44a0f82cb19a02
Mark Brobyn <Mark Brobyn>
2017-04-08 10:00:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/markb/serverwatch/ServerWatchActivity.java
@@ -0,0 +1,14 @@
+package com.example.markb.serverwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServerWatchActivity extends AppCompatActivity {
+
+    @Override
+    /*   */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_watch);
+    }
+}
